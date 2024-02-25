7eae571808612720a9b0baf2133ff5f7c6da3bad
Douglas Gilson Nunes <douglas_g_nunes@estudante.sesisenai.org.br>
2024-02-25 14:26:41 -0300

    #5

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ function nadarVerticalmente() {
     const alturaAleatoria = Math.floor(Math.random() * window.innerHeight);
     peixe.style.top = `${alturaAleatoria}px`;
 }
-alert("Seja Bem Vindo")
+
+const nome =prompt("Digite o seu nome:")
+console.log(nome)
+const idade =prompt("Digite a sua Idade:")
+console.log(nome)
+const cidade =prompt("Digite a sua cidade:")
+console.log(nome, idade, cidade)
+alert("Seja Bem Vindo: " + nome );
+
